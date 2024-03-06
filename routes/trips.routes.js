b56a827761ec0fa35e265778b133e2ca91381ae2
Laia Navalon <laianavalon@gmail.com>
2024-03-06 12:58:56 +0100

    Create endpoints in trips.routes.js

--- routes/trips.routes.js
+++ routes/trips.routes.js
@@ -1,5 +1,75 @@
 const router = require("express").Router();
+const mongoose = require("mongoose");
 
 const Trips = require("../models/Trips.model");
+
+router.get("/trips", (req, res) => {
+  Trips.find({})
+    .populate("activities")
+    .then((trips) => {
+      res.json(trips);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "Failed to retrieve trips" });
+    });
+});
+
+router.post("/trips", (req, res) => {
+  const { userId, destinations, startDate, endDate } = req.body;
+
+  Trips.create({ userId, destinations, startDate, endDate })
+    .then((newTrip) => {
+      res.json(newTrip);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "Failed to create new trip" });
+    });
+});
+
+router.get("/trips/:tripId", (req, res) => {
+  Trips.findById({ _id: req.params.tripId })
+    .populate("activities")
+    .then((tripById) => {
+      res.json(tripById);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "Failed to retrieve trip by id" });
+    });
+});
+
+router.put("/trips/:tripId", (req, res) => {
+  const { tripId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(tripId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Trips.findByIdAndUpdate(tripId, req.body, { new: true })
+    .then((updatedTrip) => res.json(updatedTrip))
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({ error: "Failed to update trip" });
+    });
+});
+
+router.delete("/trips/:tripId", (req, res) => {
+  const { tripId } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(tripId)) {
+    res.status(400).json({ message: "Specified id is not valid" });
+    return;
+  }
+
+  Trips.findByIdAndDelete(tripId)
+    .then(() => res.status(204).send())
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ error: "Failed to delete trip" });
+    });
+});
 
 module.exports = router;
